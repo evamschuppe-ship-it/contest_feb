4053d26051db7899d2c804ecfcb9eb87a84264a5
evaschuppe <evamschuppe@gmail.com>
2026-02-16 16:21:33 +0100

    Contest Lab

--- decode_start.py
+++ decode_start.py
@@ -3,4 +3,9 @@ vowel = "aeiou"
 message = ""
 with open("secret.txt") as f:
     for line in f:
-        print(line) # instead of print, you should start decoding
+        nr_vowels = 0
+        for v in vowel:
+           nr_vowels += line.count(v) #count the amt of time that particular vowel should show up
+        letter = alphabet[nr_vowels]
+        message += letter
+print(message)
